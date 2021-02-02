02bc2a308409517fe27b535e41c3ed65d667d779
Siddhant Mishra <siddhant48311@gmail.com>
2021-02-02 21:44:25 +0530

    Play and pause audio features added and video features removedd

--- app/src/main/java/com/example/justanotherapp/MainActivity.java
+++ app/src/main/java/com/example/justanotherapp/MainActivity.java
@@ -2,21 +2,26 @@ package com.example.justanotherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
 
+    public void playAudio(View view){
+        mediaPlayer.start();
+    }
+    public void pauseAudio(View view){
+        mediaPlayer.pause();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        VideoView video = (VideoView) findViewById(R.id.videoView);
-        video.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
-        video.start();
-        MediaController mediaController = new MediaController( this);
-        mediaController.setAnchorView(video);
-        video.setMediaController(mediaController);
+         mediaPlayer = MediaPlayer.create(this, R.raw.deathnote);
     }
 }
